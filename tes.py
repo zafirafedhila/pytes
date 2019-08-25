f869f073ee6aab238280294676229c12ecbf4c54
z <zafirafedhila@users.noreply.github.com>
2019-08-25 18:43:03 +0700

    Add files via upload

---
+++ tes.py
@@ -0,0 +1,57 @@
+# #list comprehension
+
+
+# # num=[0,2,3,4,5,6,7,8,9]
+# # for x in num:
+# # 	if x %2==0:
+# # 		if x % 2==0:
+# # 			even.append(x)
+
+
+# # print (even)
+
+
+
+# #No.1
+
+
+# # Buatlah sebuah program yang menhhasilkan bilangan prima 
+
+# # masukkan maksimal bilang:
+# # 0-100
+
+# N = (input('masukkan bilangan prima:'))
+# N= int((input('masukkan bilangan prima:')))
+# for x in range (2,N)
+# if (x==2 or x==3 or x==5 x==7 )
+	# print(x)
+# 	continue
+#if x%!2=0 and x==!3=0 and x
+
+
+
+
+# # listprima= ['1','3','5','7']
+# # for kata in listprima:
+# # 	jawaban = input(f'masukkan bilangan prima{kata}:')
+
+# # 	print ('\n')
+# # 	data.append()
+
+# # listKhadijah= ['nasi padang','pedas','nongkrong','ke warkop','ke mesjid','nyanyi','balap-balap','makan','nonton','jalan jalan','main sepeda','main hujan','dengar lagu','masak','main game','coklat','kopi','teh','lemon tea','hot coklat']
+# # data2= []
+# # for kata in listKhadijah:
+# # 	jawaban = input(f'apakah kamu suka{kata}:')
+
+# # 	print ('\n')
+# # 	data2.append(jawaban)
+
+# # listRydha =['data']
+# # listKhadijah =['data2']
+
+# # count=0
+# # for i in range (0, len(listRydha)):
+# # 	if listRydha[i]==listKhadijah[i]:
+# # 		count +=1
+# # print('jumlah jawaban sama:',count )
+
